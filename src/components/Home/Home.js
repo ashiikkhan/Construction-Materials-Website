edd7822fe75d695e9bafc6ef7345f6b36284f15d
ashiikkhan <ashiksust06@gmail.com>
2022-07-16 21:15:26 +0600

    almost done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,72 @@
 import React from 'react';
+import useProductData from '../../hooks/useProductData';
+import useTeamData from '../../hooks/useTeamData';
 import Slider from '../Slider/Slider';
 
 const Home = () => {
+  const [products, setProducts] = useProductData();
+  const [team, setTeam] = useTeamData();
+  const shortArr = team.slice(0, 3);
+  console.log(shortArr);
   return (
     <div className=''>
       <h2 className='text-center text-3xl font-medium md:text-4xl md:font-semibold text-gray-600 p-2 m-4'>
         We Deals with the Future of Construction
       </h2>
-      <div className=''>
+      <div className='mb-5'>
         <Slider></Slider>
+      </div>
+      <div className='my-20'>
+        <h2 className='text-center text-3xl font-medium md:text-4xl md:font-semibold text-gray-600 p-2 m-4'>
+          Our Products
+        </h2>
+
+        <div className='text-center'>
+          <div className='grid grid-cols-1 md:grid-cols-3  md:justify-items-center md:grid-rows-8'>
+            {products.map((product) => (
+              <>
+                <div className='p-5 md:p-6 m-4 bg-white shadow-xl shadow-red-200  '>
+                  <div>
+                    <img
+                      src={product.img}
+                      alt=''
+                      className='w-72 h-64 mx-auto md:w-80 md:h-64'
+                    />
+                    <h3 className='text-3xl text-gray-500 text-center p-2 '>
+                      {product.name}
+                    </h3>
+                  </div>
+                </div>
+              </>
+            ))}
+          </div>
+          <button className=' p-4 m-6 rounded-2xl shadow-2xl shadow-red-200 text-white bg-red-400 hover:scale-110 transition duration-300'>
+            Go to Products Page
+          </button>
+        </div>
+      </div>
+      <div className='my-20 text-center'>
+        <h2 className='text-center text-3xl font-medium md:text-4xl md:font-semibold text-gray-600 p-2 m-4'>
+          Our Team
+        </h2>
+        <div className='grid grid-cols-1 md:grid-cols-3  md:justify-items-center '>
+          {shortArr.map((m) => (
+            <div className='p-5 md:p-6 m-4 bg-white shadow-xl shadow-red-200  '>
+              <img
+                src={m.img}
+                alt='team-members'
+                className='w-72 h-64 mx-auto md:w-80 md:h-64'
+              />
+              <div className='p-4 text-center'>
+                <h2 className='text-2xl text-gray-500'>{m.name}</h2>
+                <h6 className='text-lg text-gray-400'>{m.designation}</h6>
+              </div>
+            </div>
+          ))}
+        </div>
+        <button className=' p-4 m-6 rounded-2xl shadow-2xl shadow-red-200 text-white bg-red-400 hover:scale-110 transition duration-300'>
+          All Team Members
+        </button>
       </div>
     </div>
   );
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -32,18 +32,37 @@ const Slider = () => {
   };
 
   return (
-    <div className='select-none flex justify-center items-center'>
-      <div className='w-full md:w-11/12 grid grid-cols-12 justify-items-center p-2'>
-        <button onClick={handleOnPreviousClick}>
-          <ChevronDoubleLeftIcon className='h-5 w-5 md:h-6 md:w-6 text-red-500 active:scale-125 md:hover:scale-125'></ChevronDoubleLeftIcon>
+    <div>
+      <div className='select-none flex justify-center items-center'>
+        <div className='w-full md:w-11/12 grid grid-cols-12 justify-items-center items-center p-2'>
+          <button
+            onClick={handleOnPreviousClick}
+            className='bg-red-400 text-white p-2 rounded-full hover:scale-110 '>
+            <ChevronDoubleLeftIcon className='h-5  w-5 md:h-6 md:w-6  active:scale-125 active:bg-white md:hover:scale-125 hidden md:block'></ChevronDoubleLeftIcon>
+          </button>
+          <img
+            className='shadow-2xl shadow-red-200 col-span-10 col-start-1 col-end-13 md:col-start-2 md:col-end-12 w-full h-64 md:w-11/12 md:h-[26rem]'
+            src={sliderImages[currentIndex].img}
+            alt=''
+          />
+          <button
+            onClick={handleOnNextClick}
+            className='bg-red-400 text-white p-2 rounded-full hover:scale-110'>
+            <ChevronDoubleRightIcon className='h-5 w-5 md:h-6 md:w-6  active:scale-125 active:bg-white md:hover:scale-125 hidden md:block'></ChevronDoubleRightIcon>
+          </button>
+        </div>
+      </div>
+      {/* button for mobile screen  */}
+      <div className='flex justify-center items-center'>
+        <button
+          onClick={handleOnPreviousClick}
+          className='p-2 mx-4 bg-red-300  rounded-full active:rounded-full hover:rounded-full active:bg-red-200 hover:bg-red-200 md:hidden'>
+          <ChevronDoubleLeftIcon className='h-5 w-5 md:h-6 md:w-6 text-white  active:scale-125 active:bg-red-200 md:hover:scale-125'></ChevronDoubleLeftIcon>
         </button>
-        <img
-          className='col-span-10 col-start-2 col-end-12 w-full md:w-11/12 md:h-[26rem]'
-          src={sliderImages[currentIndex].img}
-          alt=''
-        />
-        <button onClick={handleOnNextClick}>
-          <ChevronDoubleRightIcon className='h-5 w-5 md:h-6 md:w-6 text-red-500 md:hover:scale-125'></ChevronDoubleRightIcon>
+        <button
+          onClick={handleOnNextClick}
+          className='p-2 mx-4 bg-red-300 rounded-full active:rounded-full hover:rounded-full active:bg-red-200 hover:bg-red-200  md:hidden'>
+          <ChevronDoubleRightIcon className='h-5 w-5 md:h-6 md:w-6 text-white active:scale-125 active:bg-red-200 md:hover:scale-125'></ChevronDoubleRightIcon>
         </button>
       </div>
     </div>
